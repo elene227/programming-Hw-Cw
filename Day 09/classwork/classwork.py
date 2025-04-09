ee9aa9885d786a16e7037b6fb2f292c5f9e46797
elene227 <elelopirtskhalava@gmail.com>
2025-04-09 20:44:35 +0400

    commit

---
+++ Day 09/classwork/classwork.py
@@ -0,0 +1,11 @@
+# 1) #ლოგიკური ოპერატორების გამოყენებით შექმენით ყველა შესაძლო ვარიანტის ოპერაცია and ზეც და or ზეც, შეასრულოთ ლოგიკური ოპერაციები შედარების ოპერატორებსი გამოყენებით(<, >) ყველა შესაძლო ვარიანტი
+print (True and True)
+print (False and False)
+print (True and False)
+print (False and True)
+print (True or True)
+print (False or False)
+print (True or False)
+print (False or True)
+
+print((True or False) or (False and True) and (True and False) and (True or False) or (False and True) and (True and False))
